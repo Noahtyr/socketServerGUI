3b93b8763f59b3be2c8699b789f389a5b1139757
Juaquin <Noahdavidian@hotmail.dk>
2017-09-25 10:40:51 +0200

    Made some changes to the GUI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("socketGUI.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Chitter Chatter By Noah Juaquin");
+        primaryStage.setScene(new Scene(root, 530, 355));
         primaryStage.show();
     }
 
