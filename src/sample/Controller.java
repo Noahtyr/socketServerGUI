10da77a1eb3fd8bc03609fc175446754ce1363db
Juaquin <Noahdavidian@hotmail.dk>
2017-09-27 14:24:33 +0200

    Made Changes to the Controller, while making a method, which can Append text from a textfield into a none editable textarea via a Button!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,54 @@
 package sample;
 
 import javafx.event.ActionEvent;
-
+import	java.io.*;
+import	java.net.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+
 public class Controller {
 
+    @FXML
+    public Button sendMsg;
+    @FXML
     public TextArea chatWindow;
+    @FXML
     public TextField text;
+
+
+
+    public Socket s;
+    public OutputStream oPut;
+    public InputStream iPut;
+    public PrintWriter pw;
+
+
 
     public void buttonPress(ActionEvent event){
 
         System.out.println("User Has logged in...");
 
     }
+    @FXML
+    public void send() {
+        try {
+            String message = text.getText();
 
-    public void actionPerformed(ActionEvent event1){
-      text.getText();
+            chatWindow.appendText("\n"+text.getText() );
 
+        } catch (Exception ex){
+            ex.printStackTrace();
 
-
-
-
-
-
-
+        }
     }
-
-
-    }
+}
 
